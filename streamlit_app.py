219a0a6f216c03fe9481ed4a3965ea8269ae9b87
oritendalk <endalkorit@gmail.com>
2025-01-17 22:34:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app losers")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+print("hello world")
